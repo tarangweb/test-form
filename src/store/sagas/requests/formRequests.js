73ec7f0f1b31e1368adde6a8f7ffbb5e5fe76e77
tarang <tarang.koradiya@webcluesglobal.com>
2022-07-04 19:53:32 +0530

    google form 9

--- src/store/sagas/requests/formRequests.js
+++ src/store/sagas/requests/formRequests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001/";
+const API_BASE_URL = "https://test-form-three.vercel.app/";
 
 export const requestSaveForm = (formJson) => {
 	return axios.request({
